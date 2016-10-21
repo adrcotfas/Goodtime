68376fe2d748f177de83b6e28069fddf1a885f07
Adrian Cotfas <adrcotfas@gmail.com>
2016-10-21 15:38:50 +0300

    extract product tour strings

--- app/src/main/java/com/apps/adrcotfas/goodtime/ProductTourActivity.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/ProductTourActivity.java
@@ -16,24 +16,24 @@ public class ProductTourActivity extends IntroActivity {
          * Standard fade (like Google's intros)
          */
         addSlide(new SimpleSlide.Builder()
-                .title("Avoid distractions")
-                .description("Keep your work on track using timed sessions handled by the app.")
+                .title(R.string.intro_avoid_distractions)
+                .description(R.string.intro_avoid_distractions_description)
                 .image(R.drawable.intro01)
                 .background(R.color.intro01)
                 .backgroundDark(R.color.intro01)
                 .build());
 
         addSlide(new SimpleSlide.Builder()
-                .title("Clear your mind")
-                .description("Reward yourself with short breaks and get refreshed.")
+                .title(R.string.intro_clear_mind)
+                .description(R.string.intro_clear_mind_description)
                 .image(R.drawable.intro02)
                 .background(R.color.intro02)
                 .backgroundDark(R.color.intro02)
                 .build());
 
         addSlide(new SimpleSlide.Builder()
-                .title("Get started")
-                .description("Stay focused, get rid of procrastination and improve your productivity.")
+                .title(R.string.intro_get_started)
+                .description(R.string.intro_get_started_description)
                 .image(R.drawable.intro03)
                 .background(R.color.intro03)
                 .backgroundDark(R.color.intro03)
