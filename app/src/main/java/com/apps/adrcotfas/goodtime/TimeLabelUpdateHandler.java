d892a402722b7937cb25d50a55b3f91b40ec735a
Adrian Cotfas <adrcotfas@gmail.com>
2016-12-11 18:11:18 +0200

    fix potential crash

--- app/src/main/java/com/apps/adrcotfas/goodtime/TimeLabelUpdateHandler.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/TimeLabelUpdateHandler.java
@@ -2,21 +2,22 @@ package com.apps.adrcotfas.goodtime;
 
 import android.os.Handler;
 import android.os.Message;
-import java.lang.ref.WeakReference;
 
 class TimeLabelUpdateHandler extends Handler {
 
     private final static int UPDATE_RATE_MS = 1000;
-    private final WeakReference<TimerActivity> activity;
+    private final TimerActivity activity;
 
     TimeLabelUpdateHandler(TimerActivity activity) {
-        this.activity = new WeakReference<>(activity);
+        this.activity = activity;
     }
 
     @Override
     public void handleMessage(Message message) {
         if (TimerActivity.MSG_UPDATE_TIME == message.what) {
-            activity.get().updateTimerLabel();
+            if (activity != null){
+                activity.updateTimerLabel();
+            }
             sendEmptyMessageDelayed(TimerActivity.MSG_UPDATE_TIME, UPDATE_RATE_MS);
         }
     }
