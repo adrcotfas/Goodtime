a5ed784a645a8c0098e30351c4193cba6f017fe2
Adrian Cotfas <adrcotfas@gmail.com>
2017-01-22 23:40:16 +0200

    removed onOptionsItemSelected from AboutMainActivity

--- app/src/main/java/com/apps/adrcotfas/goodtime/about/AboutMainActivity.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/about/AboutMainActivity.java
@@ -66,16 +66,5 @@ public class AboutMainActivity extends AppCompatActivity {
                 }
             });
         }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), AboutMainActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-
     }
 }
