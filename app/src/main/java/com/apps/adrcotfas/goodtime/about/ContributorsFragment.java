897d83eb813f8bb24940f0e2e81a6b7e9d468240
Adrian Cotfas <adrcotfas@gmail.com>
2017-01-23 01:13:00 +0200

    removed AbstractContributorsFragment

--- app/src/main/java/com/apps/adrcotfas/goodtime/about/ContributorsFragment.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/about/ContributorsFragment.java
@@ -1,9 +1,16 @@
 package com.apps.adrcotfas.goodtime.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
 import com.apps.adrcotfas.goodtime.R;
 
-public class ContributorsFragment extends AbstractContributorsFragment {
+import static android.content.Intent.ACTION_VIEW;
+
+public class ContributorsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,19 @@ public class ContributorsFragment extends AbstractContributorsFragment {
         findPreference("wolfgang42").setOnPreferenceClickListener(
                 createPreferenceClickListener("https://github.com/wolfgang42")
         );
+    }
+
+    protected Preference.OnPreferenceClickListener createPreferenceClickListener(
+            final String uriString
+    ) {
+        return new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Uri uri = Uri.parse(uriString);
+                Intent intent = new Intent(ACTION_VIEW, uri);
+                startActivity(intent);
+                return true;
+            }
+        };
     }
 }
--- app/src/main/java/com/apps/adrcotfas/goodtime/about/LicencesFragment.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/about/LicencesFragment.java
@@ -1,9 +1,16 @@
 package com.apps.adrcotfas.goodtime.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
 import com.apps.adrcotfas.goodtime.R;
 
-public class LicencesFragment extends AbstractContributorsFragment {
+import static android.content.Intent.ACTION_VIEW;
+
+public class LicencesFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,19 @@ public class LicencesFragment extends AbstractContributorsFragment {
         findPreference("seekbar-pref").setOnPreferenceClickListener(
                 createPreferenceClickListener("https://github.com/MrBIMC/MaterialSeekBarPreference")
         );
+    }
+
+    protected Preference.OnPreferenceClickListener createPreferenceClickListener(
+            final String uriString
+    ) {
+        return new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Uri uri = Uri.parse(uriString);
+                Intent intent = new Intent(ACTION_VIEW, uri);
+                startActivity(intent);
+                return true;
+            }
+        };
     }
 }
