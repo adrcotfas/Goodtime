4da403df717f9181d0cbb54579c697a31d40b353
Adrian Cotfas <adrcotfas@gmail.com>
2017-05-07 10:39:07 +0300

    fix lint issue

--- app/src/main/java/com/apps/adrcotfas/goodtime/FullscreenHelper.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/FullscreenHelper.java
@@ -1,6 +1,6 @@
 package com.apps.adrcotfas.goodtime;
 
-import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.view.MotionEvent;
@@ -16,15 +16,16 @@ class FullscreenHelper {
     private ActionBar mActionBar;
 
     private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
         @Override
         public void run() {
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
         }
     };
     private final Runnable mShowPart2Runnable = new Runnable() {
@@ -86,7 +87,6 @@ class FullscreenHelper {
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    @SuppressLint("InlinedApi")
     private void show() {
         // Show the system bar
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
