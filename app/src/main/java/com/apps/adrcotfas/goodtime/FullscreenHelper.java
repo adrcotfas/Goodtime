73585b75ccf7689a608b1714abbc0919c0360b6f
Adrian Cotfas <adrcotfas@gmail.com>
2017-05-07 16:04:00 +0300

    fix fullscreen bug

--- app/src/main/java/com/apps/adrcotfas/goodtime/FullscreenHelper.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/FullscreenHelper.java
@@ -1,6 +1,6 @@
 package com.apps.adrcotfas.goodtime;
 
-import android.os.Build;
+import android.annotation.SuppressLint;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.view.MotionEvent;
@@ -16,16 +16,15 @@ class FullscreenHelper {
     private ActionBar mActionBar;
 
     private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
         @Override
         public void run() {
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            }
         }
     };
     private final Runnable mShowPart2Runnable = new Runnable() {
