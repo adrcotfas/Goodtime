6284d54777292a2e197e832eea2a1f55b943bee5
Adrian Cotfas <adrcotfas@gmail.com>
2017-12-31 12:28:10 +0200

    ensure thread safety in Room DB

--- app/src/main/java/com/apps/adrcotfas/goodtime/Database/AppDatabase.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/Database/AppDatabase.java
@@ -7,14 +7,18 @@ import android.content.Context;
 
 @Database(entities = Session.class, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase{
+
+    private static final Object LOCK = new Object();
     private static AppDatabase INSTANCE;
 
     public static AppDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
-            INSTANCE =
-                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "Goodtime")
-                            .allowMainThreadQueries()
-                            .build();
+            synchronized (LOCK) {
+                if (INSTANCE == null) {
+                    INSTANCE =  Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "Goodtime").build();
+                }
+            }
         }
         return INSTANCE;
     }
