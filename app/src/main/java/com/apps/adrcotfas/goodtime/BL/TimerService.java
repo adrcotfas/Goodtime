a49e1d234ba3b0bf7c53a7988cb9205593accbb3
Adrian Cotfas <adrcotfas@gmail.com>
2018-03-24 15:05:41 +0200

    dispose event bus observer in TimerService

--- app/src/main/java/com/apps/adrcotfas/goodtime/BL/TimerService.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/BL/TimerService.java
@@ -2,10 +2,14 @@ package com.apps.adrcotfas.goodtime.BL;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 
 import com.apps.adrcotfas.goodtime.Util.Constants;
 
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 /**
@@ -17,15 +21,26 @@ public class TimerService extends Service {
 
     private CurrentSessionManager mSessionManager;
     private NotificationHelper mNotificationHelper;
+    private Disposable mEventSubscription;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        Log.v(TAG, "onCreate");
         mNotificationHelper = new NotificationHelper(getApplicationContext());
         mSessionManager = GoodtimeApplication.getInstance().getCurrentSessionManager();
 
         setupEvents();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy");
+        if (mEventSubscription != null && !mEventSubscription.isDisposed()) {
+            mEventSubscription.dispose();
+        }
+        super.onDestroy();
     }
 
     @Override
@@ -55,7 +70,7 @@ public class TimerService extends Service {
     }
 
     private void setupEvents() {
-        GoodtimeApplication.getInstance().getBus().getEvents().subscribe(new Consumer<Object>() {
+        mEventSubscription = GoodtimeApplication.getInstance().getBus().getEvents().subscribe(new Consumer<Object>() {
             @Override
             public void accept(Object o) throws Exception {
                 if (o instanceof Constants.FinishWorkEvent) {
@@ -86,17 +101,19 @@ public class TimerService extends Service {
 
     private void onStopEvent() {
         mSessionManager.stopTimer();
+        Log.v(TAG, "onStopEvent");
         stopForeground(true);
         stopSelf();
         // TODO: store what was done of the session to ROOM
     }
 
     private void onFinishEvent(SessionType sessionType) {
-        //TODO: in continuous mode, do not stop the service
+        Log.v(TAG, "onFinishEvent");
+        // TODO: store session to ROOM
+        // TODO: in continuous mode, do not stop the service
         stopForeground(true);
         stopSelf();
         mNotificationHelper.notifyFinished(getApplicationContext(), sessionType);
-        // TODO: store session to ROOM
     }
 
     private void onSkipWorkEvent() {
