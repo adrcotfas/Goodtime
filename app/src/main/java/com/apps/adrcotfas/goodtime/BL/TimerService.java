782f2b0ad77267cb57431773d6e3cc9dfd0e2117
Adrian Cotfas <adrcotfas@gmail.com>
2018-06-10 21:43:54 +0300

    implement enable/disable WiFi and sound during work sessions

--- app/src/main/java/com/apps/adrcotfas/goodtime/BL/TimerService.java
+++ app/src/main/java/com/apps/adrcotfas/goodtime/BL/TimerService.java
@@ -2,6 +2,8 @@ package com.apps.adrcotfas.goodtime.BL;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -10,6 +12,7 @@ import com.apps.adrcotfas.goodtime.Util.Constants;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
+import static android.media.AudioManager.RINGER_MODE_SILENT;
 import static com.apps.adrcotfas.goodtime.BL.NotificationHelper.IN_PROGRESS_NOTIFICATION_ID;
 
 /**
@@ -22,6 +25,9 @@ public class TimerService extends Service {
     private CurrentSessionManager mSessionManager;
     private NotificationHelper mNotificationHelper;
     private Disposable mEventSubscription;
+
+    private int mPreviousRingerMode;
+    private boolean mPreviousWifiMode;
 
     @Override
     public void onCreate() {
@@ -89,6 +95,16 @@ public class TimerService extends Service {
     private void onStartEvent(SessionType sessionType) {
         GoodtimeApplication.getInstance().getBus().send(new Constants.ClearFinishDialogEvent());
         mSessionManager.startTimer(sessionType);
+
+        if (sessionType == SessionType.WORK) {
+            if (PreferenceHelper.isWiFiDisabled()) {
+                toggleWifi(false);
+            }
+            if (PreferenceHelper.isSoundAndVibrationDisabled()) {
+                toggleSound(false);
+            }
+        }
+
         mNotificationHelper.clearNotification();
         startForeground(IN_PROGRESS_NOTIFICATION_ID, mNotificationHelper.getInProgressBuilder(
                 mSessionManager.getCurrentSession()).build());
@@ -103,6 +119,14 @@ public class TimerService extends Service {
     private void onStopEvent() {
         mSessionManager.stopTimer();
         Log.v(TAG, "onStopEvent");
+
+        if (PreferenceHelper.isWiFiDisabled()) {
+            toggleWifi(true);
+        }
+        if (PreferenceHelper.isSoundAndVibrationDisabled()) {
+            toggleSound(true);
+        }
+
         stopForeground(true);
         stopSelf();
         // TODO: store what was done of the session to ROOM
@@ -110,6 +134,16 @@ public class TimerService extends Service {
 
     private void onFinishEvent(SessionType sessionType) {
         Log.v(TAG, "onFinishEvent");
+
+        if (sessionType == SessionType.WORK) {
+            if (PreferenceHelper.isWiFiDisabled()) {
+                toggleWifi(true);
+            }
+            if (PreferenceHelper.isSoundAndVibrationDisabled()) {
+                toggleSound(true);
+            }
+        }
+
         // TODO: store session to ROOM
         // TODO: in continuous mode, do not stop the service
         stopForeground(true);
@@ -124,6 +158,26 @@ public class TimerService extends Service {
     private void updateNotificationProgress() {
         mNotificationHelper.updateNotificationProgress(
                 mSessionManager.getCurrentSession().getDuration().getValue());
+    }
+
+    private void toggleSound(boolean restore) {
+        final AudioManager aManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (restore) {
+            aManager.setRingerMode(mPreviousRingerMode);
+        } else {
+            mPreviousRingerMode = aManager.getRingerMode();
+            aManager.setRingerMode(RINGER_MODE_SILENT);
+        }
+    }
+
+    private void toggleWifi(boolean restore) {
+        WifiManager wifiManager = (WifiManager) this.getSystemService(WIFI_SERVICE);
+        if (restore) {
+            wifiManager.setWifiEnabled(mPreviousWifiMode);
+        } else {
+            mPreviousWifiMode = wifiManager.isWifiEnabled();
+            wifiManager.setWifiEnabled(false);
+        }
     }
 
     @Override
