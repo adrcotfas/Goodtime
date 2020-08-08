e0630101ad8a210080a0262662796908f6b2985e
adrco <adrcotfas@gmail.com>
2020-08-08 22:50:48 +0300

    update paypal donation link

--- app/src/fdroid/java/com/apps/adrcotfas/goodtime/upgrade/UpgradeDialog.kt
+++ app/src/fdroid/java/com/apps/adrcotfas/goodtime/upgrade/UpgradeDialog.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
@@ -35,33 +34,14 @@ class UpgradeDialog : DialogFragment() {
     }
 
     private fun donatePayPalOnClick() {
-        val uriBuilder = Uri.Builder()
-        uriBuilder.scheme("https").authority("www.paypal.com").path("cgi-bin/webscr")
-        uriBuilder.appendQueryParameter("cmd", "_donations")
-        uriBuilder.appendQueryParameter("business", PAYPAL_USER)
-        uriBuilder.appendQueryParameter("lc", "US")
-        uriBuilder.appendQueryParameter("item_name", "${getString(R.string.app_name_long)} : ${getString(R.string.support_the_developer)}")
-        uriBuilder.appendQueryParameter("no_note", "1")
-        uriBuilder.appendQueryParameter("no_shipping", "1")
-        uriBuilder.appendQueryParameter("currency_code", PAYPAL_CURRENCY_CODE)
-        val payPalUri = uriBuilder.build()
-        val viewIntent = Intent(Intent.ACTION_VIEW, payPalUri)
-        val title: String = resources.getString(org.sufficientlysecure.donations.R.string.donations__paypal)
-        val chooser = Intent.createChooser(viewIntent, title)
-        if (viewIntent.resolveActivity(requireActivity().packageManager) != null) {
-            startActivity(chooser)
-        } else {
-            Toast.makeText(requireContext(),
-                    getString(org.sufficientlysecure.donations.R.string.donations__alert_dialog_title),
-                    Toast.LENGTH_LONG).show()
-        }
+        val url = "https://paypal.me/adrcotfas"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
         dismiss()
     }
 
     companion object {
-        private const val PAYPAL_USER = "adrcotfas@gmail.com"
-        private const val PAYPAL_CURRENCY_CODE = "EUR"
-
         fun showNewInstance(fragmentManager: FragmentManager) {
             val dialog = UpgradeDialog()
             dialog.show(fragmentManager, "")
