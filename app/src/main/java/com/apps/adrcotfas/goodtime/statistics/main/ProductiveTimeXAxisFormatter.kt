d447ba552a073ae9e8dd1761b4433f58816b5e67
Adrian Cotfas <adrcotfas@gmail.com>
2021-09-27 13:40:47 +0300

    Statistics: update productivity chart according to duration and number of sessions

--- app/src/main/java/com/apps/adrcotfas/goodtime/statistics/main/ProductiveTimeXAxisFormatter.kt
+++ app/src/main/java/com/apps/adrcotfas/goodtime/statistics/main/ProductiveTimeXAxisFormatter.kt
@@ -19,12 +19,12 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 /**
  * Custom formatter used to print the day of the week or the hour of the day.
  */
-internal class ProductiveTimeXAxisFormatter(private val mType: SpinnerProductiveTimeType, private val is24HourFormat: Boolean) :
+internal class ProductiveTimeXAxisFormatter(private val mType: ProductiveTimeType, private val is24HourFormat: Boolean) :
     ValueFormatter() {
     override fun getAxisLabel(value: Float, axis: AxisBase): String {
-        return if (mType == SpinnerProductiveTimeType.HOUR_OF_DAY && value < 24 && value >= 0) {
+        return if (mType == ProductiveTimeType.HOUR_OF_DAY && value < 24 && value >= 0) {
             StringUtils.toHourOfDay(value.toInt(), is24HourFormat)
-        } else if (mType == SpinnerProductiveTimeType.DAY_OF_WEEK && value < 7 && value >= 0) {
+        } else if (mType == ProductiveTimeType.DAY_OF_WEEK && value < 7 && value >= 0) {
             StringUtils.toDayOfWeek((value + 1).toInt())
         } else {
             ""
