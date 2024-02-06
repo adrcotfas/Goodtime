abc91a642a5e43e06de5f8faec13211eb712a5d9
adrcotfas <adrcotfas@gmail.com>
2024-02-06 20:12:03 +0200

    chore: update dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,12 +34,8 @@ android {
             isDebuggable = true
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "19"
     }
 }
 
