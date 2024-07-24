dcdefc95dc90057c4fbdf2148c0802b3f43c691f
adrcotfas <adrcotfas@gmail.com>
2024-07-24 22:12:19 +0300

    feat(StatsScreen): add top bar

--- androidApp/src/main/java/com/apps/adrcotfas/goodtime/stats/StatsScreen.kt
+++ androidApp/src/main/java/com/apps/adrcotfas/goodtime/stats/StatsScreen.kt
@@ -1,8 +1,42 @@
 package com.apps.adrcotfas.goodtime.stats
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatsScreen() {
-
+    val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+            .windowInsetsPadding(
+                WindowInsets.statusBars
+            ),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Statistics") },
+                scrollBehavior = topAppBarScrollBehavior
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+        }
+    }
 }
