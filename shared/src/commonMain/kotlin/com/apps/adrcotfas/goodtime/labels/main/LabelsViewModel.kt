db26a3cbcbde8d0ff7284a6e7080e9e485d266b8
adrcotfas <adrcotfas@gmail.com>
2024-12-26 17:48:04 +0200

    fix(LabelsViewModel): renaming active label

--- shared/src/commonMain/kotlin/com/apps/adrcotfas/goodtime/labels/main/LabelsViewModel.kt
+++ shared/src/commonMain/kotlin/com/apps/adrcotfas/goodtime/labels/main/LabelsViewModel.kt
@@ -8,17 +8,23 @@ import com.apps.adrcotfas.goodtime.data.settings.SettingsRepository
 import com.apps.adrcotfas.goodtime.ui.lightPalette
 import com.apps.adrcotfas.goodtime.utils.generateUniqueNameForDuplicate
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 data class LabelsUiState(
+    val isLoading: Boolean = true,
     val labels: List<Label> = emptyList(),
     val activeLabelName: String = Label.DEFAULT_LABEL_NAME,
     val archivedLabelCount: Int = 0,
-    val showAddEditDialog: Boolean = false,
     val labelToEditInitialName: String = "",
-    val labelToEdit: Label = Label.newLabelWithRandomColorIndex(lightPalette.lastIndex)
+    val labelToEdit: Label = Label.newLabelWithRandomColorIndex(lightPalette.lastIndex),
+    val labelNameIsValid: Boolean = true
 )
 
 val LabelsUiState.unarchivedLabels: List<Label>
@@ -32,23 +38,26 @@ class LabelsViewModel(
     private val settingsRepository: SettingsRepository
 ) : ViewModel() {
 
-    val uiState = MutableStateFlow(LabelsUiState())
+    private val _uiState = MutableStateFlow(LabelsUiState())
+    val uiState = _uiState.onStart {
+        loadData()
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), LabelsUiState())
 
-    init {
-        viewModelScope.launch {
-            repo.selectAllLabels().collect { labels ->
-                uiState.update {
-                    it.copy(
-                        labels = labels,
-                        archivedLabelCount = labels.filter { label -> label.isArchived }.size
-                    )
-                }
-            }
-        }
+    private fun loadData() {
         viewModelScope.launch {
             settingsRepository.settings.map { settings -> settings.labelName }
-                .collect { labelName ->
-                    uiState.update { it.copy(activeLabelName = labelName) }
+                .combine(repo.selectAllLabels()) { activeLabelName, labels ->
+                    activeLabelName to labels
+                }.distinctUntilChanged()
+                .collect { (activeLabelName, labels) ->
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            activeLabelName = activeLabelName,
+                            labels = labels,
+                            archivedLabelCount = labels.filter { label -> label.isArchived }.size
+                        )
+                    }
                 }
         }
     }
@@ -62,12 +71,17 @@ class LabelsViewModel(
     fun updateLabel(labelName: String, label: Label) {
         viewModelScope.launch {
             repo.updateLabel(labelName, label)
+            val isRenamingActiveLabel =
+                labelName == _uiState.value.activeLabelName && labelName != label.name
+            if (isRenamingActiveLabel) {
+                settingsRepository.activateLabelWithName(label.name)
+            }
         }
     }
 
     fun deleteLabel(labelName: String) {
         viewModelScope.launch {
-            val isDeletingActiveLabel = labelName == uiState.value.activeLabelName
+            val isDeletingActiveLabel = labelName == _uiState.value.activeLabelName
             repo.deleteLabel(labelName)
             if (isDeletingActiveLabel) {
                 settingsRepository.activateDefaultLabel()
@@ -77,7 +91,7 @@ class LabelsViewModel(
 
     fun setArchived(labelName: String, isArchived: Boolean) {
         viewModelScope.launch {
-            val isActiveLabel = uiState.value.activeLabelName == labelName
+            val isActiveLabel = _uiState.value.activeLabelName == labelName
             repo.updateLabelIsArchived(labelName, isArchived)
             if (isActiveLabel) {
                 settingsRepository.activateDefaultLabel()
@@ -92,7 +106,7 @@ class LabelsViewModel(
     }
 
     fun rearrangeLabel(fromIndex: Int, toIndex: Int) {
-        uiState.update {
+        _uiState.update {
             it.copy(labels = it.unarchivedLabels.toMutableList().apply {
                 add(toIndex, removeAt(fromIndex))
             })
@@ -101,7 +115,7 @@ class LabelsViewModel(
 
     fun rearrangeLabelsToDisk() {
         viewModelScope.launch {
-            val labelsToUpdate = uiState.value.unarchivedLabels.mapIndexed { index, label ->
+            val labelsToUpdate = _uiState.value.unarchivedLabels.mapIndexed { index, label ->
                 Pair(label.name, index.toLong())
             }
             repo.bulkUpdateLabelOrderIndex(labelsToUpdate)
@@ -110,7 +124,7 @@ class LabelsViewModel(
 
     fun duplicateLabel(name: String, isDefault: Boolean = false) {
         viewModelScope.launch {
-            uiState.update { uiState ->
+            _uiState.update { uiState ->
                 uiState.copy(labels = uiState.labels.toMutableList().apply {
                     val index =
                         indexOfFirst { it.name == if (isDefault) Label.DEFAULT_LABEL_NAME else name }
@@ -127,45 +141,45 @@ class LabelsViewModel(
 
     private fun insertLabelAt(label: Label, index: Int) {
         viewModelScope.launch {
-            uiState.update { state ->
+            _uiState.update { state ->
                 state.copy(labels = state.labels.toMutableList().apply {
                     add(index, label)
                 })
             }
             repo.insertLabelAndBulkRearrange(
                 label,
-                uiState.value.unarchivedLabels.mapIndexed { index, label ->
+                _uiState.value.unarchivedLabels.mapIndexed { index, label ->
                     Pair(label.name, index.toLong())
                 }
             )
         }
     }
 
-    fun setShowAddEditDialog(show: Boolean) {
-        uiState.update { it.copy(showAddEditDialog = show) }
-    }
-
-    fun setShowAddEditDialog(show: Boolean, labelToEdit: Label) {
-        uiState.update {
+    fun updateLabelToEditName(labelName: String) {
+        _uiState.update {
             it.copy(
-                showAddEditDialog = show,
-                labelToEditInitialName = labelToEdit.name,
-                labelToEdit = labelToEdit
+                labelToEditInitialName = labelName
             )
         }
     }
 
     fun updateLabelToEdit(label: Label) {
-        uiState.update {
+        _uiState.update {
             it.copy(labelToEdit = label)
         }
     }
 
-    fun resetLabelToEdit() {
-        uiState.update {
-            it.copy(
-                labelToEditInitialName = "",
-                labelToEdit = Label.newLabelWithRandomColorIndex(lightPalette.lastIndex)
+    fun updateLabelToEditName(
+        originalLabelName: String,
+        defaultLabelName: String,
+        newLabelName: String
+    ) {
+        _uiState.update { state ->
+            state.copy(
+                labelToEdit = state.labelToEdit.copy(name = newLabelName),
+                labelNameIsValid = newLabelName.isNotEmpty() && !state.labelNames.map { labels -> labels.lowercase() }
+                    .minus(originalLabelName.lowercase())
+                    .contains(newLabelName.lowercase()) && newLabelName.lowercase() != defaultLabelName.lowercase()
             )
         }
     }
